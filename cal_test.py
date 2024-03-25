5d920d4603983fe72ba2f66be6d51d5707da780d
Xandra Campo <xkmpera@gmail.com>
2024-03-25 16:43:49 +0100

    Separate classes in modules

--- cal_test.py
+++ cal_test.py
@@ -1,6 +1,165 @@
+"""
+Calibration steps:
+1. Select and read initial information:
+- Select measurement magnitude
+- Select radiation quality
+- Read kerma-to-operational magnitude conversion coefficient (magnitude, quality).
+- Read unit of integral and rate measurement magnitude
+- Select reference chamber (initially there will only be one, the 557, but there may be two)
+- Select quality series
+- Read chamber calibration factor for quality series
+- Read correction factor of calibration factor for quality
+
+2. Simultaneous measurements of the reference chamber and the monitoring chamber
+
+2.1. Leakage current measurements (shutter closed):
+- Select irradiation time for leakage measurements
+- Measure the electrometer charge
+- Calculate leakage current
+
+2.2. Kerma measurement (shutter open):
+- Select irradiation time for leakage measurements
+- Measure the electrometer charge
+- Measure pressure (1 barometer)
+- Measure temperature (2 probes connected to the same thermometer)
+- Calculate current intensity (excluding leakages and corrected for reference pressure and temperature conditions)
+- Calculate kerma rate (includes distance factor)
+
+3. VCV of the operational magnitude rate
+- Kerma rate (calculated previously)
+- Kerma-to-measurement magnitude conversion factor (previously chosen)
+- Electrometer range correction factor (from the reference chamber calibration certificate, RANGE LOW HIGH table, select range)
+- Air density correction factor (read from Hp tab, air attenuation table)
+
+4. Equipment measurements
+- Equipment background measurements (shutter closed)
+- Equipment readings
+- Measure pressure (only if chamber is open to air)
+- Measure temperature (only if chamber is open to air)
+- Correct for pressure and temperature if chamber is open
+- Correct for equipment background
+
+5. Calculate dose rate calibration factor
+- VCV of previously calculated magnitude between equipment reading
+- The monitoring chamber is used to verify beam stability
+- Monitoring chamber measurements should be the same when measuring with the reference chamber and when measuring with the equipment because you have the same voltage and current conditions
+- VCV * monitoring chamber measurement when measuring with the equipment
+- Equipment reading * monitoring chamber measurement when measuring with the reference chamber
+- These products are sieverts/hour * current intensity
+- The calibration factor is the relationship between both
+
+6. Distance factor
+Simultaneous measurements of the reference chamber and the monitoring chamber at two distances
+
+6.1. Leakage current measurements (shutter closed):
+- Select irradiation time for leakage measurements
+- Measure the electrometer charge
+- Calculate leakage current
+
+6.2. Current intensity measurement (shutter open):
+- Select irradiation time for leakage measurements
+- Measure the electrometer charge
+- Measure pressure (1 barometer)
+- Measure temperature (2 probes connected to the same thermometer)
+- Calculate current intensity (excluding leakages and corrected to reference pressure and temperature conditions)
+
+6.3. Calculate distance correction factor
+
+Note: All measurements: 5 measurements in 60 seconds
+"""
 import json
 
-import src.metpyx.calibration as cal
+import pandas as pd
+
+from src.metpyx.ionization_chamber import IonizationChamber, get_radiation_quality_series
+
+# 2. Simultaneous measurements of the reference chamber and the monitoring chamber
+
+# Readings of the monitoring chamber
+
+# Readings for leakage current measurements (shutter closed)
+monitor_leakage_time_readings = [60, 60, 60, 60]
+monitor_leakage_charge_readings = [-8.50E-13, -4.30E-13, -2.70E-13, -7.50E-13]
+# Readings for current measurement (shutter open)
+monitor_time_readings = [60, 60, 60, 60, 60]
+monitor_pressure_readings = [93.642, 93.642, 93.638, 93.638, 93.633]
+monitor_temperature_readings = [20.63, 20.6, 20.6, 20.6, 20.63]
+monitor_charge_readings = [4.16E-12, 3.54E-12, 4.18E-12, 4.38E-12, 4.36E-12]
+
+# Readings of the reference chamber
+
+# Readings for leakage current measurements (shutter closed)
+reference_leakage_time_readings = [60, 60, 60, 60]
+reference_leakage_charge_readings = [-1.00E-14, -1.10E-13, -1.00E-13, 3.00E-14]
+
+# Readings for kerma measurement (shutter open)
+reference_time_readings = [60, 60, 60, 60, 60]
+reference_pressure_readings = [93.642, 93.642, 93.638, 93.638, 93.633]
+reference_temperature_readings = [20.88, 20.91, 20.9, 20.94, 20.96]
+reference_charge_readings = [-1.162E-11, -1.168E-11, -1.169E-11, -1.175E-11, -1.164E-11]
+
+# Calculations
+
+# Measurements with the monitor chamber
+
+# Define monitor ionization chamber
+monitor = IonizationChamber(identification='monitor', calibrated=False, open_chamber=True)
+
+# Measure leakage current and current
+monitor_measurement = monitor.measure(
+    leakage=True, current=True, air_kerma=False,
+    leakage_time_readings=monitor_leakage_time_readings, leakage_charge_readings=monitor_leakage_charge_readings,
+    time_readings=monitor_time_readings, charge_readings=monitor_charge_readings,
+    pressure_readings=monitor_pressure_readings, temperature_readings=monitor_temperature_readings,
+    radiation_quality=None)
+
+# Measurements with the reference chamber
+
+# Read reference ionization chamber calibration coefficients and correction factors
+with open('assets/standard_chambers.json', 'r') as file:
+    data = json.load(file)
+calibration_coefficients = data['ns557']['calibration coefficient']
+corrector_factors = data['ns557']['correction factor']
+
+# Define reference ionization chamber
+reference = IonizationChamber(identification='reference', calibrated=True, open_chamber=True,
+                              calibration_coefficients=calibration_coefficients, correction_factors=corrector_factors)
+
+# Measure leakage current and air kerma
+radiation_quality = 'L-170'
+reference_measurement = reference.measure(
+    leakage=True, current=True, air_kerma=False,
+    leakage_time_readings=reference_leakage_time_readings, leakage_charge_readings=reference_leakage_charge_readings,
+    time_readings=reference_time_readings, charge_readings=reference_charge_readings,
+    pressure_readings=reference_pressure_readings, temperature_readings=reference_temperature_readings,
+    radiation_quality=radiation_quality)
+
+
+# Write results to a text file
+with open('./output.txt', 'w') as f:
+    f.write(f'IR-14D: Calibration of radiation measuring devices\n')
+    f.write(f'\n')
+    f.write(f'Reference radiation data\n')
+    f.write(f'\n')
+    f.write(f'Radiation quality: {radiation_quality}\n')
+    f.write(f'Calibration coefficient: {calibration_coefficients[get_radiation_quality_series(radiation_quality)]}\n')
+    f.write(f'Correction factor: {corrector_factors[radiation_quality]}\n')
+    f.write(f'Distance factor: {0.206378548} (WARNING! Hardcoded value)\n')
+    f.write(f'\n')
+    f.write(f'Simultaneous measurements of the reference chamber and the monitoring chamber\n')
+    f.write(f'\n')
+    f.write(f'Measurements of the reference chamber\n')
+    f.write(f'{reference_measurement.to_string(index=True)}\n')
+    f.write(f'\n')
+    f.write(f'Measurements of the monitor chamber\n')
+    f.write(f'{monitor_measurement.to_string(index=True)}\n')
+    f.write(f'\n')
+
+print("Variables written to 'output.txt'")
+
+# TODO: small discrepancy in uncertainty, compare what excel and python functions to compute the standard deviation do)
+
+# ----------------------------------------------------------------------------------------------------------------------
 
 # CLASS: CALIBRATION
 
@@ -28,83 +187,3 @@ import src.metpyx.calibration as cal
 # mmu = c.read_measurement_magnitude_units()
 # sccalf = c.read_standard_chamber_calibration_factor('assets/standard_chambers.json')
 # sccorf = c.read_standard_chamber_corrector_factor('assets/standard_chambers.json')
-
-# CLASS: IONIZATION CHAMBER
-
-# Define standard ionization chamber
-ic = cal.IonizationChamber(identification='monitor')
-
-# Measure leak current
-leak_time_readings = [60, 60, 60, 60, 0]
-leak_charge_readings = [-8.50E-13, -4.30E-13, -2.70E-13, -7.50E-13, 0]
-leak_current_series = ic.measure_current_series(time_readings=leak_time_readings, charge_readings=leak_charge_readings)
-leak_current_magnitude = ic.measure_current(time_readings=leak_time_readings, charge_readings=leak_charge_readings)
-
-# Measure current
-time_readings = [60, 60, 60, 60, 60]
-charge_readings = [4.16E-12, 3.54E-12, 4.18E-12, 4.38E-12, 4.36E-12]
-temperature_readings = [20.63, 20.6, 20.6, 20.6, 20.63]
-pressure_readings = [93.642, 93.642, 93.638, 93.638, 93.633]
-current_series = ic.measure_ambient_corrected_current_series(
-    charge_readings=charge_readings, time_readings=time_readings, leak_current_value=leak_current_magnitude.value,
-    temperature_readings=temperature_readings, pressure_readings=pressure_readings)
-current = ic.measure_ambient_corrected_current(
-    charge_readings=charge_readings, time_readings=time_readings, leak_current_value=leak_current_magnitude.value,
-    temperature_readings=temperature_readings, pressure_readings=pressure_readings)
-
-# Complete current measurement
-series_df, magnitudes_df = ic.detail_current_measurement(leak_charge_readings, leak_time_readings, charge_readings,
-                                                         time_readings, temperature_readings, pressure_readings)
-
-# Print results
-print('Monitor chamber measurements')
-print(series_df)
-print(magnitudes_df)
-
-# TODO: issue with building series dataframes: leak readings are only four, while current readings are five.
-#  If we fill with zeros, leak current returns nan and current cannot be calculated
-
-# CLASS: STANDARD IONIZATION CHAMBER
-
-# # Read standard ionization chamber calibration coefficients and correction factors
-# with open('assets/standard_chambers.json', 'r') as file:
-#     data = json.load(file)
-# standard_chamber_calibration_coefficients = data['ns557']['calibration coefficient']
-# standard_chamber_corrector_factors = data['ns557']['correction factor']
-#
-# # Define standard ionization chamber
-# sic = cal.StandardIonizationChamber(
-#     identification='ns557',
-#     calibration_coefficients=standard_chamber_calibration_coefficients,
-#     correction_factors=standard_chamber_corrector_factors
-# )
-#
-# # Leak measurement
-# leak_time = [60, 60, 60, 60, 60]
-# leak_charge = [-1.00E-14, -1.10E-13, -1.00E-13, 3.00E-14, 0]
-# leak_current_series = sic.measure_current_series(time=leak_time, charge=leak_charge)
-# leak_current = sic.measure_current(time=leak_time, charge=leak_charge)
-#
-# # Current measurement
-# time = [60, 60, 60, 60, 60]
-# pressure = [93.642, 93.642, 93.638, 93.638, 93.633]
-# temperature = [20.88, 20.91, 20.9, 20.94, 20.96]
-# charge = [-1.162E-11, -1.168E-11, -1.169E-11, -1.175E-11, -1.164E-11]
-# current_series = sic.measure_ambient_corrected_current_series(charge=charge, time=time, leak_current=leak_current.value,
-#                                                               temperature=temperature, pressure=pressure)
-# current = sic.measure_ambient_corrected_current(charge=charge, time=time, leak_current=leak_current.value,
-#                                                 temperature=temperature, pressure=pressure)
-#
-# # Air kerma measurement
-# # TODO: small discrepancy in uncertainty, compare what excel and python functions to compute the standard deviation do)
-# radiation_quality = 'L-170'
-# air_kerma_series = sic.measure_air_kerma_series(current=current_series, radiation_quality=radiation_quality)
-# air_kerma = sic.measure_air_kerma(current=current_series, radiation_quality=radiation_quality)
-#
-# # Complete current measurement
-# series_df, magnitudes_df = sic.detail_current_measurement(leak_charge, leak_time, charge, time, temperature, pressure)
-#
-# # Print results
-# print('Standard chamber measurements')
-# print(series_df)
-# print(magnitudes_df)
---
+++ src/metpyx/ionization_chamber_old.py
@@ -0,0 +1,202 @@
+import numpy as np
+import pandas as pd
+
+from src.metpyx.magnitude import series_to_magnitude, magnitudes_to_dataframe
+
+
+def get_radiation_quality_series(radiation_quality):
+    series = radiation_quality.split('-')[0]
+    return f'{series}-series'
+
+
+def results_dataframe(names, readings, magnitudes):
+
+    measurement_dict = {}
+    for name, readings, magnitude in zip(names, readings, magnitudes):
+        key = f'{name} ({magnitude.unit})'
+        value = [*readings, magnitude.value, magnitude.uncertainty, magnitude.percentage_uncertainty()]
+        measurement_dict[key] = value
+
+    df = pd.DataFrame(measurement_dict)
+
+    first_column = [f'Reading {i + 1}' for i in range(len(df) - 3)] + ['Mean', 'Uncertainty', '% Uncertainty']
+    df.insert(0, '#', first_column)
+    df.set_index('#', inplace=True)
+
+    return df
+
+
+class IonizationChamber:
+    REFERENCE_TEMPERATURE = 293.15  # K
+    REFERENCE_PRESSURE = 101.325  # kPa
+    CELSIUS_TO_KELVIN = 273.15
+
+    def __init__(self, identification, calibrated, open_chamber, calibration_coefficients=None,
+                 correction_factors=None):
+        self.identification = identification
+        self.calibrated = calibrated
+        self.open_chamber = open_chamber
+        self.calibration_coefficients = calibration_coefficients
+        self.correction_factors = correction_factors
+
+    def get_ambient_correction(self, temperature_readings, pressure_readings):
+        # Units must be: pressure in kPa, temperature in C
+        ambient_correction = (self.REFERENCE_PRESSURE / self.REFERENCE_TEMPERATURE) * (
+                self.CELSIUS_TO_KELVIN + np.array(temperature_readings)) / np.array(pressure_readings)
+        return ambient_correction
+
+    def measure_current_readings(self, charge_readings, time_readings, open_shutter, leak_current_value=None,
+                                 temperature_readings=None, pressure_readings=None):
+        # Units must be: time in s, charge in C, pressure in kPa, temperature in C, current in A
+        # If the shutter is closed, leakage current is measured (total current is measured, no leakage current is
+        # subtracted and no ambient correction is applied):
+        current = np.array(charge_readings) / np.array(time_readings)
+        # If the shutter is open and the ionization chamber is closed, total current is measured and leakage current
+        # must be subtracted (no ambient correction is applied)
+        if open_shutter:
+            current = current - leak_current_value
+            # If the shutter is open and the ionization chamber is open, total current is measured, leakage current must
+            # be subtracted and ambient correction must be applied:
+            if self.open_chamber:
+                ambient_correction = self.get_ambient_correction(temperature_readings, pressure_readings)
+                current = current * ambient_correction
+        return current
+
+    def measure_air_kerma_readings(self, current_readings, radiation_quality):
+        # Units must be: time in s, charge in C, pressure in kPa, temperature in C
+        if self.calibrated:
+            # Get ionization chamber calibration coefficient and correction factor
+            radiation_quality_series = get_radiation_quality_series(radiation_quality)
+            calibration_coefficient = self.calibration_coefficients[radiation_quality_series]
+            correction_factor = self.correction_factors[radiation_quality]
+            # Get distance factor
+            distance_factor = 0.206378548  # TODO: compute distance factor
+            # Compute air kerma readings
+            air_kerma_readings = np.array(
+                current_readings) * calibration_coefficient * correction_factor * distance_factor
+            return list(air_kerma_readings)
+        else:
+            raise Exception("Cannot compute air kerma: the ionization chamber is not calibrated.")
+
+    @staticmethod
+    def get_time(time_readings):
+        return series_to_magnitude(series=time_readings, unit='s')
+
+    @staticmethod
+    def get_charge(charge_readings):
+        return series_to_magnitude(series=charge_readings, unit='C')
+
+    @staticmethod
+    def get_temperature(temperature_readings):
+        return series_to_magnitude(series=temperature_readings, unit='ºC')
+
+    @staticmethod
+    def get_pressure(pressure_readings):
+        return series_to_magnitude(series=pressure_readings, unit='kPa')
+
+    def get_current(self, charge_readings, time_readings, open_shutter, leak_current_value=None,
+                    temperature_readings=None, pressure_readings=None):
+        current = self.measure_current_readings(charge_readings=charge_readings, time_readings=time_readings,
+                                                open_shutter=open_shutter, leak_current_value=leak_current_value,
+                                                temperature_readings=temperature_readings,
+                                                pressure_readings=pressure_readings)
+        return series_to_magnitude(series=current, unit='A')
+
+    def get_air_kerma(self, current_readings, radiation_quality):
+        air_kerma = self.measure_air_kerma_readings(current_readings=current_readings,
+                                                    radiation_quality=radiation_quality)
+        return series_to_magnitude(series=air_kerma, unit='Gy/s')
+
+    def measure_leakage(self, leakage_charge_readings, leakage_time_readings):
+        # Leakage current measurements (closed shutter)
+        leakage_current_readings = self.measure_current_readings(
+            charge_readings=leakage_charge_readings, time_readings=leakage_time_readings, open_shutter=False)
+        leakage_time = self.get_time(time_readings=leakage_time_readings)
+        leakage_charge = self.get_charge(charge_readings=leakage_charge_readings)
+        leakage_current = self.get_current(time_readings=leakage_time_readings, charge_readings=leakage_charge_readings,
+                                           open_shutter=False)
+
+        # Define lists to build dataframe
+        magnitudes = [leakage_time, leakage_charge, leakage_current]
+        names = ['Leakage time', 'Leakage charge', 'Leakage current']
+        readings = [leakage_time_readings, leakage_charge_readings, leakage_current_readings]
+
+        # Build dataframe
+        measurements_df = results_dataframe(names, readings, magnitudes)
+
+        return measurements_df
+
+    def measure_current(self, charge_readings, time_readings, temperature_readings, pressure_readings, leakage_current_value):
+
+        # Total current measurements (open shutter)
+        current_readings = self.measure_current_readings(
+            charge_readings=charge_readings, time_readings=time_readings, open_shutter=True,
+            leak_current_value=leakage_current_value, temperature_readings=temperature_readings,
+            pressure_readings=pressure_readings)
+        time = self.get_time(time_readings=time_readings)
+        pressure = self.get_pressure(pressure_readings=pressure_readings)
+        temperature = self.get_temperature(temperature_readings=temperature_readings)
+        charge = self.get_charge(charge_readings=charge_readings)
+        current = self.get_current(
+            charge_readings=charge_readings, time_readings=time_readings, open_shutter=True,
+            leak_current_value=leakage_current_value, temperature_readings=temperature_readings,
+            pressure_readings=pressure_readings)
+
+        # Define lists to build dataframe
+        magnitudes = [time, pressure, temperature, charge, current]
+        names = ['Time', 'Pressure', 'Temperature', 'Charge', 'Current']
+        readings = [time_readings, pressure_readings, temperature_readings, charge_readings, current_readings]
+
+        # Build dataframe
+        measurements_df = results_dataframe(names, readings, magnitudes)
+
+        return measurements_df
+
+    def measure_air_kerma(self, current_readings, radiation_quality):
+        # Air kerma measurements (open shutter)
+        air_kerma_readings = self.measure_air_kerma_readings(current_readings=current_readings,
+                                                             radiation_quality=radiation_quality)
+        air_kerma = self.get_air_kerma(current_readings=current_readings, radiation_quality=radiation_quality)
+
+        # Define lists to build dataframe
+        magnitudes = [air_kerma]
+        names = ['Air kerma']
+        readings = [air_kerma_readings]
+
+        # Build dataframe
+        measurements_df = results_dataframe(names, readings, magnitudes)
+
+        return measurements_df
+
+    def measure(self, leakage, current, air_kerma, leakage_time_readings=None, leakage_charge_readings=None,
+                time_readings=None, charge_readings=None, pressure_readings=None, temperature_readings=None,
+                radiation_quality=None):
+        if leakage:
+            # Leakage current measurements (closed shutter)
+            leakage = self.measure_leakage(leakage_charge_readings=leakage_charge_readings,
+                                           leakage_time_readings=leakage_time_readings)
+            measurement = leakage
+            if current:
+                # Total current measurements (open shutter)
+                leakage_current_value = leakage.loc['Mean', 'Leakage current (A)']
+                current = self.measure_current(
+                    charge_readings=charge_readings, time_readings=time_readings, temperature_readings=temperature_readings,
+                    pressure_readings=pressure_readings, leakage_current_value=leakage_current_value)
+                measurement = pd.merge(leakage, current, left_index=True, right_index=True, how='outer')
+                custom_order = [f'Reading {i + 1}' for i in range(len(measurement) - 3)] + ['Mean', 'Uncertainty', '% Uncertainty']
+                measurement = measurement.reindex(custom_order)
+                if air_kerma:
+                    # Air kerma measurements (open shutter)
+                    current_readings = current['Current (A)']
+                    current_readings = current_readings.iloc[:-3]
+                    air_kerma = self.measure_air_kerma(current_readings=current_readings, radiation_quality=radiation_quality)
+                    measurement = pd.merge(measurement, air_kerma, left_index=True, right_index=True, how='outer')
+                    custom_order = [f'Reading {i + 1}' for i in range(len(measurement) - 3)] + ['Mean', 'Uncertainty', '% Uncertainty']
+                    measurement = measurement.reindex(custom_order)
+            return measurement
+        else:
+            msg = (f'Options\n'
+                   f'Measure leakage current: leakage=True, current=False, air_kerma=False\n'
+                   f'Measure current: leakage=True, current=True, air_kerma=False\n'
+                   f'Measure air kerma: leakage=True, current=True, air_kerma=True\n')
+            raise Exception(msg)
