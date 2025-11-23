ef50329b5e67002ada33379d9d5c0538c84eed6c
Xandra Campo <xkmpera@gmail.com>
2025-11-23 15:58:03 +0100

    Update package __init__.py

--- src/metpyx/__init__.py
+++ src/metpyx/__init__.py
@@ -1,7 +1,5 @@
 # MetPyX public API
 
-from .qualities import XrayQualities
-from .quantities import XrayQuantities
-from .sensitive_spectrum import SensitiveSpectrum, resolve_quality
+from .data import Qualities, Quantities
 
-__all__ = ['XrayQualities', 'XrayQuantities', 'SensitiveSpectrum', 'resolve_quality']
+__all__ = ['Qualities', 'Quantities']
