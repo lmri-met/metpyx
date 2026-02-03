3be8d5931544aa2cb821995a5f5b78104ca4c0d1
Xandra Campo <xkmpera@gmail.com>
2026-02-03 18:53:16 +0100

    Add handlers for default or custom coefficients (mu_tr_over_rho_air and h_K)

--- src/metpyx/data/__init__.py
+++ src/metpyx/data/__init__.py
@@ -1,7 +1,7 @@
 # Expose common data classes at package level for convenient imports
-# This allows: from metpyx.data import Qualities, OperationalQuantities
+# This allows: from metpyx.data import Qualities, OperationalQuantities, get_mu_tr_over_rho_air, get_h_k
 
 from .qualities import Qualities
-from .quantities import OperationalQuantities
+from .quantities import OperationalQuantities, get_mu_tr_over_rho_air, get_h_k
 
-__all__ = ["Qualities", "OperationalQuantities"]
+__all__ = ["Qualities", "OperationalQuantities", "get_mu_tr_over_rho_air", "get_h_k"]
